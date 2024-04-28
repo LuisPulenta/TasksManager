d82743983ff9016d3f65eb5076a9d3c181ea7011
LuisPulenta <luisalbertonu@gmail.com>
2024-04-28 18:33:48 -0300

    AddDb

---
+++ TasksManager.BackEnd/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasksManager.Shared.Entities;
+
+namespace TasksManager.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MyTask> MyTasks { get; set; }
+    }
+}
