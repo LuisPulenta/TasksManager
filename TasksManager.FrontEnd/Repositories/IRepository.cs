e32606665f4ebca7fb772ee8184359e9f4243a4e
LuisPulenta <luisalbertonu@gmail.com>
2024-05-01 11:51:41 -0300

    TaskCreate

--- TasksManager.FrontEnd/Repositories/IRepository.cs
+++ TasksManager.FrontEnd/Repositories/IRepository.cs
@@ -5,5 +5,8 @@ namespace TasksManager.FrontEnd.Repository
     public interface IRepository
     {
         Task<Response<T>> GetAsync<T>(string url);
+        Task<Response<T>> PostAsync<T>(string url, T model);
+        Task<Response<T>> PutAsync<T>(string url, T model);
+        Task<Response<T>> DeleteAsync<T>(string url);
     }
 }
--- TasksManager.FrontEnd/Repositories/Repository.cs
+++ TasksManager.FrontEnd/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using TasksManager.FrontEnd.Repository;
 using TasksManager.Shared.Models;
 
@@ -13,6 +14,13 @@ namespace TasksManager.FrontEnd.Repositories
             _httpClient = httpClient;
         }
 
+        //-----------------------------------------------------------------------------------------------------
+        private JsonSerializerOptions _jsonDefaultOptions => new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        //-----------------------------------------------------------------------------------------------------
         public async Task<Response<T>> GetAsync<T>(string url)
         {
             var responseHttp = await _httpClient.GetAsync(url);
@@ -26,12 +34,81 @@ namespace TasksManager.FrontEnd.Repositories
             }
             
             var responseString = await responseHttp.Content.ReadAsStringAsync();
-            var responseJson=JsonSerializer.Deserialize<T>(responseString);
+            var responseJson=JsonSerializer.Deserialize<T>(responseString, _jsonDefaultOptions);
 
             return new Response<T>
             {
                 IsSuccess = true,
                 Result=responseJson
+            };
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        public async Task<Response<T>> PostAsync<T>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model, _jsonDefaultOptions);
+            var messageContent=new StringContent(messageJson, Encoding.UTF8,"application/json");
+            var responseHttp = await _httpClient.PostAsync(url, messageContent);
+            if(!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to post the object"
+                };
+            }
+
+            var responseString = await responseHttp.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<T>(responseString, _jsonDefaultOptions);
+
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Result = responseJson
+            };
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        public async Task<Response<T>> PutAsync<T>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model, _jsonDefaultOptions);
+            var messageContent = new StringContent(messageJson, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContent);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to put the object"
+                };
+            }
+
+            var responseString = await responseHttp.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<T>(responseString, _jsonDefaultOptions);
+
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Result = responseJson
+            };
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        public async Task<Response<T>> DeleteAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.DeleteAsync(url);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to delete the object"
+                };
+            }
+
+            return new Response<T>
+            {
+                IsSuccess = true,
             };
         }
     }
